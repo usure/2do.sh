1e7953217e628e3213ab090debaf9a4bdf72d9ff
Greg X <gamedevguy12@gmail.com>
2011-11-26 13:03:27 -0500

    new file:   githelp 	new file:   todo.sh

---
+++ todo.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+file=~/todo.txt
+
+if [ -f $file ]
+	then
+echo file exists  /dev/null >> /dev/null 2>&1
+	else
+touch file
+fi
+
+while :
+	do
+
+if [ ! -s $file ]; then
+    echo "$file is empty" >> /dev/null 2>&1
+else
+echo "Your current todo.txt"
+cat $file
+sleep 4
+fi
+
+echo "What would you like to add to your todo list?"
+read todotext 
+echo $todotext >> $file
+
+echo "Do you want to add anything else?"
+read yesorno
+addmore=$yesorno
+
+if [ "$yesorno" == "yes" ]; then
+echo "What would you like to add to your todo list?"  >> /dev/null 2>&1
+#read todotext 
+#echo $todotext >> todo.txt
+  elif [ "$yesorno" == "no" ]; then
+exit
+  fi
+
+done
+
+
